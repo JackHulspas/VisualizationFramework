f50de88b983b2f11d548d824664e08e72e8bb76c
= <=>
2023-11-25 13:05:34 +0100

    Added the GLAD library

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,10 +1,39 @@
 #include <visualizationFramework/runner.h>
 #include <iostream>
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 //Main function
 int main(void)
 {
     std::cout << "Hello world" << std::endl;
     Runner runner;
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }    
+
+    glViewport(0, 0, 800, 600);
+
+    while(!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();    
+    }
+
     return 0;
 }
--- visualizationFramework/runner.cpp
+++ visualizationFramework/runner.cpp
@@ -1,12 +1,9 @@
 #include "runner.h"
-#include <GLFW/glfw3.h>
 
 //Constructor
 Runner::Runner()
 {
     std::cout << "Hello world 2" << std::endl;
-
-    if(glfwInit()) std::cout << "GLFW initialized" << std::endl;
 }
 
 //Destructor
